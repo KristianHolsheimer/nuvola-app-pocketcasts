03542ff950c5163b393777649ca196474169f064
Kristian Holsheimer <kristian.holsheimer@gmail.com>
2016-01-28 00:10:37 +0100

    edited maintainer_link and email address

--- integrate.js
+++ integrate.js
@@ -1,5 +1,5 @@
 /*
- * Copyright 2015 Your name <your e-mail>
+ * Copyright 2016 Kristian Holsheimer <kristian.holsheimer@gmail.com>
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
